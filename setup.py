4dcd55e6ba32fdb5fa19da5a8ee94db85fa8c0aa
iptL-F4ck <76460968+iptL-F4ck@users.noreply.github.com>
2022-05-29 19:56:47 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='LibcSearcherX',
+    version='0.0.1',
+    author='iptL',
+    author_email='iptL_htb@163.com',
+    url='',
+    description='Online Libc Searcher',
+    packages=['LibcSearcherX'],
+    install_requires=['requests'],
+    entry_points={
+        'console_scripts': [
+        ]
+    }
+)
